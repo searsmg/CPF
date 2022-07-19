f190d320b3c57756e7076a25f48607222643f1b8
searsmg <megan.sears@colostate.edu>
2022-07-19 10:23:50 -0600

    quick manual discharge calcs

---
+++ q_manual_processing.R
@@ -0,0 +1,16 @@
+
+# process manual discharge measurements
+
+library(tidyverse)
+
+setwd("D:/CPF/manual_Q")
+
+q <- read_csv("D:/CPF/manual_Q/tunnel_q_20220708.csv") %>%
+  mutate(dist_m = dist_m - lag(dist_m)) %>%
+  replace(is.na(.), 0) %>%
+  mutate(depth_m = depth_cm / 100) %>%
+  select(-depth_cm) %>%
+  mutate(q_m3s = dist_m * depth_m * v_ms) 
+
+q_total_m3s <- sum(q$q_m3s)
+q_Ls <- q_total_m3s * 1000
